6709a453329e0f31c98be0470c01abb0fac2e9ea
juliasut <juliasutygina@gmail.com>
2021-07-04 11:09:22 -0600

    handle error message if data not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,9 +6,12 @@ const breedFetcher = function (breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(err, res, body) {
   // The code above submits an HTTP GET request
     if(err) {
-      return (`${err}: status code ${res.statusCode}`);
+      throw (err);
     }
     const data = JSON.parse(body);
+    if (!data || data.length === 0) {
+      return callback(`Breed ${breedName} not found`)
+    }
     const description = data[0].description;
     // Need a callback for async request, otherwise will return before it's fulfilled
     return callback(description)
@@ -16,7 +19,7 @@ const breedFetcher = function (breedName, callback) {
 };
 
 
-const printOutCatBreed = (breed, description) => {
-  console.log(breed + description)
+const printOutCatBreed = (breed, description, err) => {
+  err ? console.log(`Breed ${breedName} not found`) : console.log(breed + description)
 };
 breedFetcher(breedName, printOutCatBreed);
