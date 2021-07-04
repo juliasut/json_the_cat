3432064e1c017941d1ed416400b6acc8c904ef8d
juliasut <juliasutygina@gmail.com>
2021-07-04 11:35:49 -0600

    refactor the logic to index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,19 @@
 const request = require('request');
 let breedName = process.argv.slice(2);
 
-const breedFetcher = function (breedName, callback) {
+const fetchBreedDescription = function (breedName, callback) {
 
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(err, res, body) {
-  // The code above submits an HTTP GET request
-    if(err) {
-      throw (err);
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, res, body) {
+  
+    if(error) {
+      return callback(error);
     }
     const data = JSON.parse(body);
     if (!data || data.length === 0) {
       return callback(`Breed ${breedName} not found`)
     }
-    const description = data[0].description;
-    // Need a callback for async request, otherwise will return before it's fulfilled
-    return callback(description)
+    return callback(data[0].description)
   });
 };
 
-
-const printOutCatBreed = (breed, description, err) => {
-  err ? console.log(`Breed ${breedName} not found`) : console.log(breed + description)
-};
-breedFetcher(breedName, printOutCatBreed);
+module.exports = { fetchBreedDescription };
